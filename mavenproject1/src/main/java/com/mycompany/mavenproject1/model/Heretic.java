29a5c4f91113eb25d65ebe601bb764d71a14e5ae
katyalika <lihachevak05@gmail.com>
2025-02-26 12:13:46 +0300

    объявлены переменные для класса Еретик

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/model/Heretic.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/model/Heretic.java
@@ -4,10 +4,24 @@
  */
 package com.mycompany.mavenproject1.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author lihac
  */
 public class Heretic {
+    public String name;
+    public ArrayList<String> crime = new ArrayList<String>();
+    public String place;
+    public int id;
+    public String danger_level;
+    public static ArrayList<String> d_levels = new ArrayList<String>() {{
+        add("Low"); add("Medium"); add("High"); add("Critical");
+    }};
     
+    
+    public void Heretic() {
+        
+    }
 }
